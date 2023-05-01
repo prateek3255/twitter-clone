58d217de00893501a4f37ae045d61606f6bb09f8
Prateek Surana <prateeksurana3255@gmail.com>
2023-05-01 20:31:29 +0530

    :sparkles: Add follower/following count

--- apps/nextjs/app/[username]/page.tsx
+++ apps/nextjs/app/[username]/page.tsx
@@ -2,9 +2,17 @@ import { BackButton } from "ui/icons";
 import { Button } from "ui";
 import Image from "next/image";
 
+const FollowCount = ({ count, label }: { count: number; label: string }) => (
+  <a href="/psuranas/followers" className="hover:underline decoration-white">
+    <span className="text-white font-bold text-sm">{count} </span>
+    <span className="text-gray-500 text-sm">{label}</span>
+  </a>
+);
+
 export default function Profile() {
   return (
-    <div className="w-full max-w-[600px] border-r border-solid border-gray-700">
+    <div className="w-full h-full max-w-[600px] border-r border-solid border-gray-700">
+      {/* Header */}
       <div className="h-14 w-full px-4 flex items-center">
         <div className=" min-w-[56px] flex items-center">
           <button className="text-white p-2 rounded-full hover:bg-gray-100/10">
@@ -16,12 +24,14 @@ export default function Profile() {
           <span className="text-gray-500 text-sm">1,153 Tweets</span>
         </div>
       </div>
+      {/* Cover Image */}
       <Image
         src="https://pbs.twimg.com/profile_banners/723196595722809345/1682895978/1500x500"
         width={600}
         height={200}
         alt="cover image"
       />
+      {/* Profile Image */}
       <div className="relative pt-3 px-4">
         <div className="absolute top-[-67px] left-4">
           <Image
@@ -32,10 +42,26 @@ export default function Profile() {
             alt="Prateek's profile image"
           />
         </div>
-        <div className=" h-[70px] flex w-full justify-end">
-            <Button variant="teritary">
-                Edit profile
-            </Button>
+        <div className=" h-[67px] flex w-full justify-end">
+          <Button variant="teritary">Edit profile</Button>
+        </div>
+        {/* Name */}
+        <div className="flex flex-col mb-3">
+          <span className="text-white text-xl font-extrabold">
+            Prateek Surana
+          </span>
+          <span className="text-gray-500 text-sm">@psuranas</span>
+        </div>
+        {/* Bio */}
+        <div>
+          <span className="text-white text-sm">
+            Building Devfolio | Frontend Engineer | Writing @ prateeksurana.me
+          </span>
+        </div>
+        {/* Follower/Following count */}
+        <div className="flex gap-5 mt-3">
+          <FollowCount count={1466} label="Following" />
+          <FollowCount count={698} label="Followers" />
         </div>
       </div>
     </div>
