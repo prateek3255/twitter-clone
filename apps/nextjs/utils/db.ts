2d0f88e5905c14f78c086e9019d87b6a4e4e23b5
Prateek Surana <prateeksurana3255@gmail.com>
2023-09-03 17:53:29 +0530

    :sparkles: Add db

--- apps/nextjs/utils/db.ts
+++ apps/nextjs/utils/db.ts
@@ -1,5 +1,5 @@
 import 'server-only';
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from 'database'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
---
+++ apps/remix/app/routes/_auth.signup.tsx
@@ -0,0 +1,69 @@
+import { ButtonOrLink } from "components/ButtonOrLink";
+import { FloatingInput } from "ui";
+import { Link } from "@remix-run/react";
+
+export default function Signup() {
+  return (
+    <>
+      <h1 className="font-bold text-3xl text-white mb-7">
+        Create your account
+      </h1>
+      <form>
+        <div className="flex flex-col gap-4 mb-10">
+          <div className="flex flex-col sm:flex-row w-full gap-4">
+            <FloatingInput
+              required
+              autoFocus
+              label="Name"
+              id="name"
+              name="name"
+              maxLength={40}
+              placeholder="John"
+            />
+            <FloatingInput
+              required
+              label="Username"
+              id="username"
+              name="username"
+              minLength={2}
+              maxLength={15}
+              pattern="^[a-zA-Z0-9_]{1,15}$"
+              placeholder="johndoe"
+            />
+          </div>
+          <FloatingInput
+            required
+            type="email"
+            label="Email"
+            id="email"
+            name="email"
+            placeholder="john@doe.com"
+          />
+          <FloatingInput
+            type="password"
+            label="Password"
+            id="password"
+            name="password"
+            required
+            placeholder="********"
+          />
+        </div>
+
+        <ButtonOrLink type="submit" size="large" stretch>
+          Sign In
+        </ButtonOrLink>
+
+        <ButtonOrLink variant="secondary" size="large" className="w-full mt-4">
+          Sign Up with a burner account
+        </ButtonOrLink>
+
+        <div className="text-center text-white text-base w-full mt-4 font-medium">
+          Already have an account?{" "}
+          <Link to="/signin" className=" underline" prefetch="intent">
+            Sign in
+          </Link>
+        </div>
+      </form>
+    </>
+  );
+}
