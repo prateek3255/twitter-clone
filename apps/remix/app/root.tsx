23713fc74dcf9b7dcd7af0eb1481ebc4d5a09f59
Prateek Surana <prateeksurana3255@gmail.com>
2023-09-17 19:06:25 +0530

    :sparkles: Add global 404 page

--- apps/remix/app/root.tsx
+++ apps/remix/app/root.tsx
@@ -7,6 +7,8 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
 } from "@remix-run/react";
 import { ButtonOrLink } from "~/components/ButtonOrLink";
 import { destroyUserSession } from "~/utils/auth.server";
@@ -84,6 +86,30 @@ export default function App() {
 }
 
 export const ErrorBoundary = () => {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error) && error.status === 404) {
+    return (
+      <Document>
+        <main className="flex h-full w-full justify-center items-center">
+          <div className="max-w-md w-full border border-solid border-gray-400 rounded-xl px-10 pt-5 pb-8">
+            <div className="w-100 flex justify-center mb-4 text-white">
+              <TwitterLogo aria-label="Twitter logo" />
+            </div>
+            <div className="flex flex-col gap-8 items-center">
+              <h1 className="font-bold text-2xl text-white">
+                Hmm...this page doesn’t exist. Try searching for something else.
+              </h1>
+              <ButtonOrLink size="large" className="w-full" as="link" to="/">
+                Go to home
+              </ButtonOrLink>
+            </div>
+          </div>
+        </main>
+      </Document>
+    );
+  }
+
   return (
     <Document>
       <main className="flex h-full w-full justify-center items-center">
