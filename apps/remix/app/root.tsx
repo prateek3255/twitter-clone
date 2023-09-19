dd90e2a0d24d779879d3dbe427dbb67e69c7bf7b
Prateek Surana <prateeksurana3255@gmail.com>
2023-09-19 09:43:11 +0530

    :sparkles: Add icons to remix

--- apps/remix/app/root.tsx
+++ apps/remix/app/root.tsx
@@ -59,6 +59,23 @@ const Document = ({ children }: { children: React.ReactNode }) => {
     <html lang="en" className="h-full">
       <head>
         <meta charSet="utf-8" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <style
           dangerouslySetInnerHTML={{
